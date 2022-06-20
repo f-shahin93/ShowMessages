303ab9d27e2d0101073ccc6fa0fd7cdebb60111c
F_Shahin <f.shahin.kh@gmail.com>
2022-06-20 23:54:38 +0430

    Removed unused code.

--- app/src/main/java/com/shahin/showmessages/ui/pager/PagerAdapter.kt
+++ app/src/main/java/com/shahin/showmessages/ui/pager/PagerAdapter.kt
@@ -7,12 +7,6 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 class PageAdapter(fragment: Fragment, val fragments: List<Fragment>) :
     FragmentStateAdapter(fragment) {
 
-    /*var pages: List<Fragment> = pages.toList()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }*/
-
     override fun getItemCount(): Int = fragments.size
 
     override fun createFragment(position: Int): Fragment = fragments[position]
--- app/src/main/java/com/shahin/showmessages/ui/pager/SavedMessagesView.kt
+++
@@ -1,8 +0,0 @@
-package com.shahin.showmessages.ui.pager
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun SavedMessages(){
-
-}
