a772191b78d1be90b08233611c1d1439e12351b7
F_Shahin <f.shahin.kh@gmail.com>
2022-06-20 13:19:21 +0430

    Create compose base fragment

--- app/src/main/java/com/shahin/showmessages/ui/base/BaseFragment.kt
+++ app/src/main/java/com/shahin/showmessages/ui/base/BaseFragment.kt
@@ -4,10 +4,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.CallSuper
 import androidx.annotation.LayoutRes
+import androidx.compose.ui.platform.ComposeView
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+
+
+abstract class BaseComposeFragment : Fragment() {
+
+    private var _composeRootView: ComposeView? = null
+    protected val composeRootView get() = _composeRootView!!
+
+    @CallSuper
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val view = ComposeView(inflater.context).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            _composeRootView = this
+        }
+        _composeRootView = view
+        return view.rootView
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _composeRootView = null
+    }
+
+}
 
 
 abstract class BaseFragment<B : ViewDataBinding>(@LayoutRes layoutId: Int) : Fragment(layoutId) {
